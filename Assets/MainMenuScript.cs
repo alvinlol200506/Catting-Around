2c669f5e130a480bf85bb23faa0a40b8458fa2ea
CrimsonsTempest <rafaeldevenang@gmail.com>
2025-09-03 10:21:52 +0700

    half ass main menu

---
+++ Assets/MainMenuScript.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+
+    public void Settings() { }
+    public void Exit()
+    {
+        Application.Quit();
+     }
+}

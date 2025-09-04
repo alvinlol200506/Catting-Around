0f4fe7fef371b4b82f79c1c96957e1bbcb3667a8
CrimsonsTempest <rafaeldevenang@gmail.com>
2025-09-04 11:17:43 +0700

    custom ending connect

--- Assets/EndingScript.cs
+++ Assets/EndingScript.cs
@@ -1,17 +1,32 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Diagnostics;
+using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class EndingScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] public List<Sprite> images = new List<Sprite>();
+     [SerializeField] GameObject EndingPanel;
+    UnityEngine.UI.Image imagenya;
     void Start()
     {
+
+        imagenya = EndingPanel.GetComponent<UnityEngine.UI.Image>();
         String death = DeathData.causeOfDeath;
 
         switch (death)
         {
             case "High":
+                imagenya.sprite = images[2];
+                break;
+
+            default:
+                imagenya.sprite = images[0];
                 break;
 
         }
@@ -26,3 +41,4 @@ public class EndingScript : MonoBehaviour
 
     
 }
+

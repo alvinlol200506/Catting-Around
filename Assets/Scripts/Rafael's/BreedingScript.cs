d7e600b0d7be44b5f2757dac80b28467c521a833
CrimsonsTempest <rafaeldevenang@gmail.com>
2025-09-05 20:59:19 +0700

    Love

--- Assets/Scripts/Rafael's/BreedingScript.cs
+++ Assets/Scripts/Rafael's/BreedingScript.cs
@@ -4,6 +4,13 @@ using UnityEngine.SceneManagement;
 public class BreedingScript : InteractionTrigger
 {
 
+
+    [SerializeField] GameObject lope1;
+     [SerializeField] GameObject lope2;
+     [SerializeField] GameObject lope3;
+     [SerializeField] GameObject lope4;
+
+
     int BreedCount;
     public EndingImagesSO so;
     public override void InteractTrigger()
@@ -16,6 +23,16 @@ public class BreedingScript : InteractionTrigger
     public void Breed()
     {
         BreedCount++;
+
+        if (BreedCount == 1)
+            lope1.SetActive(true);
+        else if (BreedCount == 2)
+            lope2.SetActive(true);
+        else if (BreedCount == 3)
+            lope3.SetActive(true);
+        else if (BreedCount == 4)
+            lope4.SetActive(true);            
+
         if (BreedCount >= 5)
         {
             so.ending = "Mating";
